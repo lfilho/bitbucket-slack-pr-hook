84ced9459fc07c5ff4b60de2ea530b917874d5cf
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2014-09-27 16:01:12 -0300

    Setting up for tests: jsons and busterjs

---
+++ test/bitbucketParser-test.js
@@ -0,0 +1,12 @@
+var buster = require('buster');
+var bitbucketParser = require('../lib/bitbucketParser');
+
+// Make some functions global for BDD style
+buster.spec.expose();
+var expect = buster.expect;
+
+describe('A module', function () {
+    it('states the obvious', function () {
+        expect(true).toEqual(true);
+    });
+});
---
+++ test/buster.js
@@ -0,0 +1,9 @@
+var config = module.exports;
+
+config['default'] = {
+    rootPath: '../',
+    environment: 'node',
+    sources: ['lib/**/*.js'],
+    tests: ['test/*-test.js']
+};
+
